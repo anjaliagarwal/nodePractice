c96cea17946187564545c2355e144112676e4e1d
Anjali <anji.agarwal12@gmail.com>
2025-02-19 15:14:06 +0530

    operation

---
+++ operation/op.js
@@ -0,0 +1,12 @@
+var num1 = 10;
+var num2 = 5;
+
+var sum = num1 + num2;
+var difference = num1 - num2;
+var product = num1 * num2;
+var quotient = num1/num2;
+
+console.log('Sum :', sum );
+console.log('Sub:', difference );
+console.log('Mul :', product );
+console.log('Div :', quotient );
